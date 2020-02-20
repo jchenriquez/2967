e675eaea0df92a2b61c3d5b2bf69396abb433ecf
jchenriquez <jc.henriquez04@gmail.com>
2020-02-20 13:31:22 -0500

    using binary search left and right of j to look for closest sum.

--- threesumclosest.go
+++ threesumclosest.go
@@ -5,6 +5,23 @@ import (
 	"sort"
 )
 
+func setResult(arr []int, result *int, sumSoFar int, target int) {
+
+	ind := sort.Search(len(arr), func(i int) bool {
+		return target <= sumSoFar+arr[i]
+	})
+
+	if ind < 0 {
+		ind++
+	} else if ind >= len(arr) {
+		ind--
+	}
+
+	if int(math.Abs(float64((sumSoFar+arr[ind])-target))) < int(math.Abs(float64((*result)-target))) {
+		*result = sumSoFar + arr[ind]
+	}
+}
+
 // ThreeSumClosest will return the closest sum to the target
 func ThreeSumClosest(nums []int, target int) int {
 
@@ -14,9 +31,12 @@ func ThreeSumClosest(nums []int, target int) int {
 	result := math.MaxInt32
 
 	for i := 0; i < len(nums); i++ {
-
 		for j := i + 1; j < len(nums); j++ {
-
+			sumed := nums[i] + nums[j]
+			setResult(nums[i+i:j], &result, sumed, target)
+			setResult(nums[j:], &result, sumed, target)
 		}
 	}
+
+	return result
 }
